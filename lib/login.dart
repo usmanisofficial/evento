7c74293329299a53be5396e4dc24141cf2223dc7
Muhammad Usman <usmanisofficial@outlook.com>
2022-07-28 02:20:31 +0500

    Working on Buttons

--- lib/login.dart
+++ lib/login.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    double p = 17;
     return SafeArea(
       child: Material(
         child: Padding(
@@ -24,23 +27,112 @@ class LoginPage extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                  enabledBorder: InputBorder.none,
-                  focusedBorder: InputBorder.none,
-                  fillColor: Colors.grey[200],
-                  filled: true,
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  prefixIcon: const Icon(
-                    Icons.email,
-                    color: Colors.grey,
+              Form(
+                  child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
                   ),
-                  hintText: "Input Email",
-                  hintStyle: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.w600),
-                ),
-              ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: const Padding(
+                        padding: EdgeInsets.only(left: 40, right: 10),
+                        child: Icon(
+                          CupertinoIcons.mail,
+                          size: 25,
+                          color: Color.fromARGB(255, 146, 146, 146),
+                        ),
+                      ),
+                      fillColor: const Color.fromRGBO(243, 244, 247, 1),
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                        borderSide: const BorderSide(
+                          color: Colors.white,
+                        ),
+                      ),
+                      hintText: "Input Email",
+                      hintStyle: TextStyle(
+                          color: const Color.fromARGB(255, 146, 146, 146),
+                          fontSize: 18,
+                          fontFamily: GoogleFonts.poppins().fontFamily,
+                          fontWeight: FontWeight.w600),
+                      isDense: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        fillColor: const Color.fromRGBO(243, 244, 247, 1),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                          borderSide: const BorderSide(
+                            color: Colors.white,
+                          ),
+                        ),
+                        hintText: "Input Password",
+                        hintStyle: const TextStyle(
+                            color: Color.fromARGB(255, 146, 146, 146),
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600),
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        contentPadding: EdgeInsets.fromLTRB(40, p, p, p)),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 146, 146, 146),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        minimumSize: MaterialStateProperty.all(
+                          const Size(double.infinity, 50),
+                        ),
+                        fixedSize: MaterialStateProperty.all(
+                            Size.fromWidth(MediaQuery.of(context).size.width)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ))),
+                    child: const Text("Login"),
+                  ),
+                ],
+              ))
             ],
           ),
         ),
